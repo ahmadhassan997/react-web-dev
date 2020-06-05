9251494cb178745f08b605ac2b2edb4f5a7a3f11
ahmadhassan997 <riemann997@gmail.com>
2020-06-05 22:28:53 +0500

    single page application

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 
@@ -14,6 +18,9 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     };
   }
 
@@ -25,7 +32,18 @@ class Main extends Component {
 
     const HomePage = () => {
         return (
-            <Home />
+            <Home dish={ this.state.dishes.filter((dish) => dish.featured)[0] } 
+                promotion={ this.state.promotions.filter((promo) => promo.featured)[0] }
+                leader={ this.state.leaders.filter((leader) => leader.featured)[0] }
+            />
+        );
+    }
+
+    const DishWithId = ({match}) => {
+        return (
+            <Dishdetail dish={ this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0] }
+                comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10)) }
+            />
         );
     }
 
@@ -35,6 +53,8 @@ class Main extends Component {
         <Switch>
             <Route path="/home" component={HomePage} />
             <Route exact path="/menu" component={() => <Menu dishes={ this.state.dishes } />} />
+            <Route path="/menu/:dishId" component={DishWithId} />
+            <Route exact path="/contactus" component={Contact} />
             <Redirect to="/home" />
         </Switch>
         <Footer />
